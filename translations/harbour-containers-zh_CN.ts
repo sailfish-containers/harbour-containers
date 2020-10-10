93161ae75696182fc9f729a639715d901a5e5ec8
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2020-10-11 02:08:47 +0800

    Create harbour-containers-zh_CN.ts

---
+++ translations/harbour-containers-zh_CN.ts
@@ -0,0 +1,252 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../qml/pages/About.qml" line="17"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="34"/>
+        <source>&lt;i&gt; A Linux containers manager for SailfishOS &lt;/i&gt;</source>
+        <translation>这是一个旗鱼系统 Linux 容器管理器</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="53"/>
+        <source>donate</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="64"/>
+        <source>Inspired by Preflex&apos;s (TMO &quot;Xwayland victory!&quot;) and elros34&apos;s (Github &quot;sailfish_linux_chroot&quot;) awesome work</source>
+        <translation>该项目受到 Preflex 的 TMO 上的 Xwayland victory! 及 elros31 的 Github 的 sailfish_linux_chroot 这些优秀作品的启发。</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/About.qml" line="68"/>
+        <source>This project is proudly licensed under &lt;b&gt;GNU GPLv3.&lt;/b&gt;</source>
+        <translation>该项目自豪地采用了 GNU GpLv3 许可证。</translation>
+    </message>
+</context>
+<context>
+    <name>ContainerSettings</name>
+    <message>
+        <location filename="../qml/pages/ContainerSettings.qml" line="24"/>
+        <source>Settings: </source>
+        <translation>设置:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerSettings.qml" line="56"/>
+        <source>Container</source>
+        <translation>容器</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerSettings.qml" line="61"/>
+        <source>Change icon</source>
+        <translation>修改图标</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerSettings.qml" line="80"/>
+        <source>Destroy container</source>
+        <translation>销毁容器</translation>
+    </message>
+</context>
+<context>
+    <name>ContainerSnapshots</name>
+    <message>
+        <location filename="../qml/pages/ContainerSnapshots.qml" line="30"/>
+        <source>Snapshots: </source>
+        <translation>快照:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerSnapshots.qml" line="74"/>
+        <source>Restore</source>
+        <translation>恢复</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerSnapshots.qml" line="78"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+</context>
+<context>
+    <name>ContainerView</name>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="46"/>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="52"/>
+        <source>Snapshots</source>
+        <translation>快照</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="58"/>
+        <source>Unfreeze</source>
+        <translation>取消冻结</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="58"/>
+        <source>Freeze</source>
+        <translation>冻结</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="139"/>
+        <source>Details</source>
+        <translation>详情</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="147"/>
+        <source>name:</source>
+        <translation>名称:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="150"/>
+        <source>state:</source>
+        <translation>状态:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="154"/>
+        <source>rootfs:</source>
+        <translation>rootfs:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="158"/>
+        <source>pid:</source>
+        <translation>pid:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="161"/>
+        <source>cpu use:</source>
+        <translation>CPU使用:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="164"/>
+        <source>memory use:</source>
+        <translation>存储使用:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="167"/>
+        <source>kmem use:</source>
+        <translation>kmem使用:</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="173"/>
+        <location filename="../qml/pages/ContainerView.qml" line="249"/>
+        <source>Session</source>
+        <translation>会话控制</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="180"/>
+        <source>attach</source>
+        <translation>附上</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="191"/>
+        <source>X session</source>
+        <translation>X会话</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="208"/>
+        <source>mountpoints</source>
+        <translation>挂载点</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="241"/>
+        <source>advanced controls</source>
+        <translation>高级控制</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="254"/>
+        <source>run onboard</source>
+        <translation>运行 onboard 虚拟键盘</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="261"/>
+        <source>kill Xwayland</source>
+        <translation>杀死进程</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="269"/>
+        <source>Setup</source>
+        <translation>安装</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="275"/>
+        <source>setup xsession</source>
+        <translation>安装 xsession</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/ContainerView.qml" line="283"/>
+        <source>init container config</source>
+        <translation>初始化容器配置</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="26"/>
+        <source>&lt;b&gt;Containers&lt;/b&gt;</source>
+        <translation>这是容器</translation>
+    </message>
+</context>
+<context>
+    <name>CreateDialog</name>
+    <message>
+        <location filename="../qml/dialogs/CreateDialog.qml" line="122"/>
+        <source>Setup container&apos;s xsession, depending on the device&apos;s spec it may require long time to complete. Be sure to have network access and enough battery charge. Currently only debian based systems are supported by the scripts. Read more: https://github.com/sailfish-containers/lxc-templates-desktop/wiki/Desktop</source>
+        <translation>设置容器的 xsession ，取决于设备的配置，可能需要很长时间才能完成。请确保你的设备始终连接到网络并且电脑充足。当前脚本仅支持基于 debian 的系统。请参阅下方网址 https://github.com/sailfish-containers/lxc-templates-desktop/wiki/Desktop</translation>
+    </message>
+</context>
+<context>
+    <name>IconPickerDialog</name>
+    <message>
+        <location filename="../qml/dialogs/IconPickerDialog.qml" line="14"/>
+        <source>Select an icon: </source>
+        <translation>选择一个图标:</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="98"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="102"/>
+        <source>Stop all</source>
+        <translation>全部停止</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="106"/>
+        <source>Freeze all</source>
+        <translation>全部冻结</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="118"/>
+        <source>Containers</source>
+        <translation>容器</translation>
+    </message>
+</context>
+<context>
+    <name>MissingRequirements</name>
+    <message>
+        <location filename="../qml/pages/MissingRequirements.qml" line="11"/>
+        <source>error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MissingRequirements.qml" line="21"/>
+        <source>Oh no! Your device doesn&apos;t seems to support LXC!</source>
+        <translation>抱歉！你的设备似乎不支持LXC！</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MissingRequirements.qml" line="28"/>
+        <source>Read more</source>
+        <translation>阅读更多</translation>
+    </message>
+</context>
+</TS>
